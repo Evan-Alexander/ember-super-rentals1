eac42aaaf221efd03fcf93dee80346ae3a950b20
Jason Brown and Xia Amendolara <xia@>
2017-03-22 10:07:44 -0700

    delete review function now working

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -26,6 +26,19 @@ export default Ember.Route.extend({
          return rental.save();
        });
        this.transitionTo('rental', rental);
+     },
+     destroyReview(review) {
+       review.destroyRecord();
+       this.transitionTo('index');
+     },
+     destroyRental(rental) {
+       var review_deletions = rental.get('reviews').map(function(review) {
+         return review.destroyRecord();
+       });
+       Ember.RSVP.all(review_deletions).then(function(){
+         return rental.destroyRecord();
+       });
+       this.transitionTo('index');
      }
   }
 });
